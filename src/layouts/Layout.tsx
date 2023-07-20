d33cfef37028599bb3fa69e6aac3cd4b7742d8d3
suhag-alamin <developersuhag@gmail.com>
2023-07-20 12:13:42 +0600

    footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 import Navbar from './Navbar';
-import React from 'react';
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="py-16 px-8 lg:px-16">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
